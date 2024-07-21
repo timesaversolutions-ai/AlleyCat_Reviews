9fd572b27da14211294308ffda268c5c3b215fba
justinlearnsDS <justin.black@tsaversolutions.net>
2024-07-21 10:57:56 -0700

    clear out unused code

--- list_models.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-set API_KEY=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
-
-curl https://api.openai.com/v1/models ^
-  -H "Authorization: Bearer %API_KEY%"
--- parse_transcription.bat
+++
@@ -1,3 +0,0 @@
-node parse_response.js
-
-pause
